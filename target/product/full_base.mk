Project-1CE/build_make

commit 1c91d5c03aefb9bc9c0c94553ca1aac40a9a04f7

product: Remove default AOSP sounds
We'll ship a different collection of sounds in our vendor module.

Change-Id: I30383b8fd75c1171ce3f4c5059d75995c0399bad
Signed-off-by: alk3pInjection <webmaster@raspii.tech>

target/product/full_base.mk
@@ -42,16 +42,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
-# Additional settings used in all AOSP builds
-PRODUCT_VENDOR_PROPERTIES := \
-    ro.config.ringtone?=Ring_Synth_04.ogg \
-    ro.config.notification_sound?=pixiedust.ogg
-
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
-
-# Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)

commit fe4fcfeff2b8c95fd98342b915cbc2b66e982dbc

fixup! product: Remove default AOSP sounds
Guard it properly.

Fixes: 1c91d5c ("product: Remove default AOSP sounds")
Change-Id: I8988f7de6a06981674c58223297ec5743cc293d1

target/product/full_base.mk
@@ -42,6 +42,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+ifeq ($(ICE_BUILD),)
+# Additional settings used in all AOSP builds
+PRODUCT_VENDOR_PROPERTIES := \
+    ro.config.ringtone?=Ring_Synth_04.ogg \
+    ro.config.notification_sound?=pixiedust.ogg
+
+# Get some sounds
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+endif
+
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 